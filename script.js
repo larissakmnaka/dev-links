78e74ff67f602e4d71ac40768d9ecc43eefcbf9f
Larissa Nakamura <larissakmnakamura@gmail.com>
2025-05-31 22:12:56 -0300

    Ajustes

--- script.js
+++ script.js
@@ -9,18 +9,11 @@ function toggleMode() {
   // }
 
   const img = document.querySelector("#profile img")
-  console.log(img)
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/perfil2.HEIC")
-  } else {
-    img.setAttribute("src", "./assets/perfil1.HEIC")
-  }
-
-  const alt = document.querySelector("#profile img")
-  console.log(img)
-  if (html.classList.contains("light")) {
     img.setAttribute("alt", "Foto de uma japonesa sorrindo de óculos, com a cabeça meio inclinada")
   } else {
+    img.setAttribute("src", "./assets/perfil1.HEIC")
     img.setAttribute("alt", "Foto de uma japonesa sorrindo de óculos, com a cabeça reta")
   }
 }
